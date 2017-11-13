458b5d1acec700720faa354fb7b6390452e0424f
Gabriele Valastro <33549583+valagab@users.noreply.github.com>
2017-11-13 23:57:28 +0100

    Add files via upload

---
+++ exercise_medium.py
@@ -0,0 +1,11 @@
+# 3) Crea una classe Human con attributi 'firstname' e 'lastname'. Inoltre dovrà avere l'attributo 'my_pet' che è 
+#       un'istanza della classe MyPet. La classe deve avere un metodo 'say_hello' che stampa a video il nome e il cognome
+#       della persona e il nome dell'animale, insieme al risultato della funzione 'say_hello' dell'oggetto my_pet
+
+# Creare una repository e per ogni esercizio fare un commit in una branch separata e fare il merge su master alla fine di ogni esercizio.
+# Ogni classe deve stare su un file separato.
+
+# Voti:
+# - Bah: non ne fai neanche uno
+# - Ci siamo quasi: ne fai due
+# - Boss: li fai tutti e 3
